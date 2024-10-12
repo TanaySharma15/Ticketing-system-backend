b7bf11cb908c2d28431fb0a2fcbf9fdaaff73685
TanaySharma15 <tanaysharma1526@gmail.com>
2024-10-12 17:21:50 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect("MONGO_URL");
-        // console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB");
     } catch (err) {
         console.error("Error connecting to MongoDB: ", err);
-        process.exit(1); // Exit process with failure
+        process.exit(1); 
     }
 };
 

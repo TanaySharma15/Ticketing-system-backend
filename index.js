d599fa3ec36eaf55e13e21c88bb989b97457d1ea
TanaySharma15 <tanaysharma1526@gmail.com>
2024-10-12 17:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-// const express = require('express');
-// require('dotenv').config()
-// const mongoose = require('mongoose')
 
-// const app = express();
-// app.use(express.json());
-// const PORT = process.env.PORT;
-
-
-// mongoose.connect(process.env.MONGO_URL).then(()=>{
-//     console.log("Connected to mongodb");
-// }).catch((err)=>{
-//     console.log("error : "+err);
-// })
-
-
-
-// app.listen(PORT,()=>{
-//     console.log(`server is running on port ${PORT}`)
-// })
 const express = require('express');
 require('dotenv').config();
-const connectDB = require('./config/db'); // Adjust the path if necessary
+const connectDB = require('./config/db');
 const authRoutes = require('./routes/auth');
 const ticketRoutes = require('./routes/tickets');
 process.env.NODE_ENV = 'test';
@@ -29,9 +10,9 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT;
 
-connectDB(); // Call the function to connect to MongoDB
+connectDB(); 
 
-app.use('/api/auth', authRoutes); // Adjust the path as needed
+app.use('/api/auth', authRoutes); 
 app.use('/api/ticket',ticketRoutes)
 
 app.listen(PORT, () => {

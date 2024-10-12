59aaea394d058f342527f0c4ffe9d6b444d42bd0
TanaySharma15 <tanaysharma1526@gmail.com>
2024-10-12 17:19:35 +0530

    Update ticket.js

--- models/ticket.js
+++ models/ticket.js
@@ -8,32 +8,5 @@ const ticketSchema = new mongoose.Schema({
     destination:String,
     date:Number 
 })
-// const ticketSchema = new mongoose.Schema({
-//     title: {
-//         type: String,
-//         required: true,
-//     },
-//     description: {
-//         type: String,
-//         required: true,
-//     },
-//     status: {
-//         type: String,
-//         enum: ['open', 'in-progress', 'closed'],
-//         default: 'open',
-//     },
-//     createdBy: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User', // Reference to the User model
-//         required: true,
-//     },
-//     assignedTo: {
-//         type: mongoose.Schema.Types.ObjectId,
-//         ref: 'User', // Reference to the User model
-//     },
-// }, {
-//     timestamps: true, // Automatically add createdAt and updatedAt fields
-// });
-
 const Ticket = mongoose.model('Ticket',ticketSchema);
 module.exports = Ticket
